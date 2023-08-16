b0c3948280b7faf92fead950d8400fcf8ac336d7
Fi Amanillah <mdbitu35@gmail.com>
2023-08-16 15:34:02 +0600

    1

--- 1/Rough.js
+++ 1/Rough.js
@@ -1,6 +1,7 @@
 const myObj = {
     name: "John",
     age: 30,
+    font:" master ≡",
     cars: [
       {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
       {name:"BMW", models:["320", "X3", "X5"]},
@@ -8,3 +9,6 @@ const myObj = {
     ]
 };
 
+console.log(myObj.font)
+
+// document.getElementById("demo").innerHTML = myObj.font;
