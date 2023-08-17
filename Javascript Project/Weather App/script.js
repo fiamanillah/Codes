c4c40f6bbf9bfdb326ad9bd0f1c022324c3cb567
Fi Amanillah <mdbitu35@gmail.com>
2023-08-17 23:17:06 +0600

    Add Weather App project

---
+++ Javascript Project/Weather App/script.js
@@ -0,0 +1,69 @@
+
+
+const search = document.querySelector(".search input");
+const btn = document.querySelector(".search button");
+
+
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const apiKey = "41151816b71880d54ede817918a91253";
+
+async function checkWeather(city){
+
+
+   
+
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let data = await response.json();
+
+
+
+    if (response.status == 404) {
+        document.getElementById("err").style.display= "block";
+        document.querySelector(".weather").style.display ="none";
+    }else{
+        document.querySelector("#temp").innerHTML= data.main.temp + `°C`;
+        document.querySelector("#humidity").innerHTML= data.main.humidity + `%`;
+        document.querySelector("#wind").innerHTML= data.wind.speed + ` km/h`;
+        document.querySelector("#city").innerHTML= data.name;
+
+        if (data.weather[0].main == "Clear") {
+            document.querySelector(".temp img").src = "images/clear.png"
+        }else if(data.weather[0].main == "Clouds"){
+            document.querySelector(".temp img").src = "images/clouds.png"
+        }else if(data.weather[0].main == "Drizzle"){
+            document.querySelector(".temp img").src = "images/drizzle.png"
+        }else if(data.weather[0].main == "Mist"){
+            document.querySelector(".temp img").src = "images/mist.png"
+        }else if(data.weather[0].main == "Rain"){
+            document.querySelector(".temp img").src = "images/rain.png"
+        }else if(data.weather[0].main == "Snow"){
+            document.querySelector(".temp img").src = "images/snow.png"
+        }else if(data.weather[0].main == "Haze"){
+            document.querySelector(".temp img").src = "images/mist.png"
+        }
+    
+        document.querySelector(".weather").style.display ="block";
+        document.getElementById("err").style.display= "none";
+    }
+
+    
+
+
+    console.log(data);
+
+}
+
+
+
+btn.addEventListener("click", () => {
+    checkWeather(search.value);
+})
+
+
+search.addEventListener("keypress", (event)=>{
+    if (event.key === "Enter") {
+        event.preventDefault();
+        btn.click();
+      }
+})
--- Learning Javascript/Rough.js
+++ Learning Javascript/Rough.js
@@ -1,14 +1,3 @@
-const myObj = {
-    name: "John",
-    age: 30,
-    font:" master ≡",
-    cars: [
-      {name:"Ford", models:["Fiesta", "Focus", "Mustang"]},
-      {name:"BMW", models:["320", "X3", "X5"]},
-      {name:"Fiat", models:["500", "Panda"]}
-    ]
-};
 
-console.log(myObj.font)
 
-// document.getElementById("demo").innerHTML = myObj.font;
+console.log("Hello")
